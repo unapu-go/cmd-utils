1a5596bfb4aea197746079dcddf460f50b38f132
Moises P. Sena <moisespsena@gmail.com>
2021-08-18 23:02:10 -0300

    add StdIO paths to defaults STDIN

--- io.go
+++ io.go
@@ -3,6 +3,7 @@ package cmdu
 import (
 	"os"
 	"path/filepath"
+	"strings"
 
 	ph "github.com/moisespsena-go/path-helpers"
 )
@@ -17,15 +18,35 @@ const (
 
 func (typ StdIOType) Get(pth string, defaul ...*os.File) (f *os.File, err error) {
 	switch typ {
-	case StdOut, StdErr:
-		switch pth {
-		case "STDOUT":
+	case StdOut:
+		switch strings.ToUpper(pth) {
+		case "STD", "STDOUT":
 			return os.Stdout, nil
 		case "STDERR":
 			return os.Stderr, nil
+		case "_":
+			return os.Open(os.DevNull)
+		}
+	case StdErr:
+		switch strings.ToUpper(pth) {
+		case "STDOUT":
+			return os.Stdout, nil
+		case "STD", "STDERR":
+			return os.Stderr, nil
+		case "_":
+			return os.Open(os.DevNull)
+		}
+	case StdIn:
+		switch strings.ToUpper(pth) {
+		case "STD", "STDIN":
+			return os.Stdin, nil
+		case "_":
+			return os.Open(os.DevNull)
 		}
 	}
+
 	var sysDef bool
+
 	if len(defaul) == 0 {
 		sysDef = true
 		defaul = append(defaul, nil)
